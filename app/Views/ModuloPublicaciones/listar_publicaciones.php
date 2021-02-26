867c2c5379b2da2a941a23e4f2c211caf8900363
SebastianLB96 <75597047+SebastianLB96@users.noreply.github.com>
2021-02-26 13:47:28 -0500

    Vista lista de publicaciones

--- app/Views/ModuloPublicaciones/listar_publicaciones.php
+++ app/Views/ModuloPublicaciones/listar_publicaciones.php
@@ -5,13 +5,52 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Contenido</h1>
+            <h1 class="m-0">Publicaciones</h1>
           </div><!-- /.col -->
           <div class="col-12">
-            <p> Este es el espacio para la vista de Listar Publicaciones </p>
+            <div class="card">
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                    <th>Id</th>
+                    <th>Titulo</th>
+                    <th>Tipo</th>
+                    <th>Vendedor</th>
+                    <th>Fecha de publicación</th>
+                    <th class="text-info">Opciones</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div><!-- /.content-header -->
   </div>
+  <script>
+  $(function () {
+    $("#example1").DataTable({
+      "language": {"url": "//cdn.datatables.net/plug-ins/1.10.22/i18n/Spanish.json"},
+      "dom": 'Bfrtip',
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
   
