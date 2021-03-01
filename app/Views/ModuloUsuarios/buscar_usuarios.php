51cdc55984e6e681b2978699735482b2c63705dd
andrea-iu <66572146+andrea-iu@users.noreply.github.com>
2021-02-28 21:27:05 -0500

    modal editar

--- app/Views/ModuloUsuarios/buscar_usuarios.php
+++ app/Views/ModuloUsuarios/buscar_usuarios.php
@@ -44,6 +44,38 @@
     </div><!-- /.container-fluid -->
   </div><!-- /.content-header -->
 </div>
+<div class="modal fade " id="modal-default">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Editar Datos Usuarios</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+           <div class="form-group">
+             <label for="exampleInputEmail1">Documento</label>
+             <input type="email" class="form-control" id="documento_edit" name="documento_edit" disabled="">
+           </div>
+            <div class="form-group">
+              <label>Estado</label>
+              <select class="form-control" name="estado_edit">
+                <option value="Activo">Activo</option>
+                <option value="Inactivo">Inactivo</option>
+                <option value="Pendiente">Pendiente</option>
+              </select>
+            </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+          <button type="button" class="btn btn-primary" data-dismiss="modal">Guardar cambios</button>
+        </div>
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
 <script>
 
 $(document).ready(iniciar);
@@ -88,6 +120,18 @@ function listarusuarios() {
     }   
   });
 }
+$(function() {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000
+    });
 
+    $('.toastrDefaultSuccess').click(function() {
+      toastr.success('Se ha eliminado correctamente')
+    });
+   
+  });
 
 </script>
