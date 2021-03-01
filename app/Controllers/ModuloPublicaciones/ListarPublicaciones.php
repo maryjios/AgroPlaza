d845fbd3407e4073f41078bb5d162320873885b6
SebastianLB96 <75597047+SebastianLB96@users.noreply.github.com>
2021-03-01 00:59:22 -0500

    publicaciones controller

--- app/Controllers/ModuloPublicaciones/ListarPublicaciones.php
+++ app/Controllers/ModuloPublicaciones/ListarPublicaciones.php
@@ -2,16 +2,24 @@
 
 use CodeIgniter\Controller;
 use App\Controllers\BaseController;
+use App\Models\PublicacionesModel;
 
 class ListarPublicaciones extends BaseController {
 
-	public function index(){
+	public function index($estado = 'ACTIVA'){
+		$publicaciones = new PublicacionesModel();
+
+		$datos['resultados'] = $publicaciones->select('*','usuarios.nombres','usuarios.apellidos')
+							   ->where('estado_publicacion',$estado)
+							   ->join('usuarios','publicaciones.id_usuario = usuarios.id')
+							   ->findall();
+
 		$data['modulo_selected'] = "Publicaciones";
 		$data['opcion_selected'] = "ListarPublicaciones";
 
 
 		echo view('template/header', $data);
-		echo view('ModuloPublicaciones/listar_publicaciones');
+		echo view('ModuloPublicaciones/listar_publicaciones',$datos);
 		echo view('template/footer');
 	}
 
