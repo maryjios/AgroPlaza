135a0b3d5652550789982a4244400283d5779f8b
Russelt30 <alkirang30@gmail.com>
2021-03-01 19:58:25 -0500

    modal unidades

---
+++ app/Models/UnidadesModel.php
@@ -0,0 +1,24 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CiudadesModel extends Model {
+    protected $table      = 'ciudad';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = ['nombre', 'id_departamento'];
+
+    protected $useTimestamps = false;
+    protected $createdField  = '';
+    protected $updatedField  = '';
+    protected $deletedField  = '';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
