3041c267677bc19425ee15c5a59d16b3c2d4c6a7
Alvaro Bedoya <alkirang30@gmail.com>
2021-03-18 15:06:15 -0500

    cambios modal

--- app/Views/ModuloPublicaciones/unidades.php
+++ app/Views/ModuloPublicaciones/unidades.php
@@ -51,7 +51,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel3">Registrar Unidad</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form id="form_nuevoUni" method="post" action="#" autocomplete="of">
@@ -66,7 +66,7 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
                         <button type="submit" class="btn btn-primary">Guardar</button>
                     </div>
                 </form>
